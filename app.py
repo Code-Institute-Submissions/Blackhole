e8bac02ed504ea4e03773290fe3d19a2b41be8d4
DFife99 <Devlan.fife2@gmail.com>
2020-10-01 14:07:41 +0000

    removed the index page so itll load straight up at the login page, adding some design elements to make the page more appealing and adding in a card for logging in

--- app.py
+++ app.py
@@ -10,11 +10,6 @@ app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
-
-
 @app.route('/login')
 def login():
     return render_template('login.html')
