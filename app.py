42eb8959d3bed8c6f3db001547bf07afd123ae27
DFife99 <Devlan.fife2@gmail.com>
2020-10-03 13:59:00 +0000

    added basic functionality for registering an account

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask import (
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
+if os.path.exists('env.py'):
+    import env
 
 
 app = Flask(__name__)
@@ -13,6 +15,8 @@ app.config['MONGO_DBNAME'] = os.environ.get('MONGO_DBNAME')
 app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
 app.secret_key = os.environ.get('SECRET_KEY')
 
+mongo = PyMongo(app)
+
 
 @app.route('/')
 @app.route('/login')
@@ -20,8 +24,26 @@ def login():
     return render_template('login.html')
 
 
-@app.route('/signup')
+@app.route('/signup', methods=["GET", "POST"])
 def signup():
+    if request.method == 'POST':
+        # check if username exsists
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get('username').lower()})
+
+        if existing_user:
+            flash('Username already exists')
+            return redirect(url_for('signup'))
+
+        signup = {
+        "username": request.form.get('username').lower(),
+        "password": generate_password_hash(request.form.get('password'))
+        }
+        mongo.db.users.insert_one(signup)
+        # put the user into session
+        session['user'] = request.form.get('username').lower()
+        flash('Registered Successfully')
+        return redirect(url_for('login'))
     return render_template('signup.html')
 
 
