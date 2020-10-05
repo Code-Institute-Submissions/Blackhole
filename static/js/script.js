b5b7c63e1f005c355495f0b586c300a491586397
DFife99 <Devlan.fife2@gmail.com>
2020-10-05 03:46:02 +0000

    almost finishd styling new post section, just need to fix a few bugs, added extra divs because push-1 wasnt working for an unknown reason, need to fix animation also

--- static/js/script.js
+++ static/js/script.js
@@ -5,4 +5,17 @@ $(document).ready(function(){
         autoClose: true,
         yearRange: 100,
     });
-  });
+});
+
+// need to fix for clicking else where 
+$(document).on('click', '#acitivity-post', function() {
+    $('#label-post').toggle();
+})
+
+$(document).on('click', '#image-post-icon', function() {
+    $('#image-post').click();
+})
+
+$(document).on('click', '#mentions-post-icon', function() {
+    $('#mentions-post').click();
+})
